a13da764f7f171c39a4e06fe93d94564b4b652da
Мишанов Алексей Борисович <ABoMishanov@sberbank.ru>
2023-11-07 10:39:11 +0300

    Initial commit

---
+++ src/main/java/com/sbt/Task6Application.java
@@ -0,0 +1,11 @@
+package com.sbt;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class Task6Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Task6Application.class, args);
+    }
+}
